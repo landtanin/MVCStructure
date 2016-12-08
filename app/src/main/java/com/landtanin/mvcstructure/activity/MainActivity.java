240bb342d13ed48e8c6a3943d99ed8a49403b9cd
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-12-08 19:42:55 +0000

    second way to put fragment in Activity

--- app/src/main/java/com/landtanin/mvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/landtanin/mvcstructure/activity/MainActivity.java
@@ -1,10 +1,11 @@
 package com.landtanin.mvcstructure.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.landtanin.mvcstructure.R;
+import com.landtanin.mvcstructure.fragment.MainFragment;
 import com.landtanin.mvcstructure.util.ScreenUtils;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         int screenHeight = ScreenUtils.getInstance().getScreenHeight();
 
         Toast.makeText(this, "Width = " + screenWidth, Toast.LENGTH_LONG).show();
+
+        if (savedInstanceState == null) {
+            // First Created
+            // Place Fragment here
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer, new MainFragment())
+                    .commit();
+
+
+        }
 
     }
 }
