b6099cc625f66865eeb7597b70e7a18b6bef443e
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-12-08 21:16:45 +0000

    finish ChangeFragment1/2

--- app/src/main/java/com/landtanin/mvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/landtanin/mvcstructure/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.landtanin.mvcstructure.activity;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.landtanin.mvcstructure.R;
@@ -9,6 +12,7 @@ import com.landtanin.mvcstructure.fragment.MainFragment;
 import com.landtanin.mvcstructure.util.ScreenUtils;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +28,46 @@ public class MainActivity extends AppCompatActivity {
             // First Created
             // Place Fragment here
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contentContainer, new MainFragment())
+                    .add(R.id.contentContainer,
+                            MainFragment.newInstance(123),
+                            "MainFragment")
                     .commit();
-
 
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_second_fragment) {
+
+            Toast.makeText(this, "Second Fragment", Toast.LENGTH_SHORT).show();
+
+            // Handled
+            return true;
+        }
+
+        // Not handled
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+
+            MainFragment fragment = (MainFragment)
+                    getSupportFragmentManager().findFragmentByTag("MainFragment");
+            fragment.setHelloText("Woooo");
+
+        }
     }
 }
--- app/src/main/java/com/landtanin/mvcstructure/fragment/MainFragment.java
+++ app/src/main/java/com/landtanin/mvcstructure/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.landtanin.mvcstructure.R;
 
@@ -15,14 +16,39 @@ import com.landtanin.mvcstructure.R;
 
 public class MainFragment extends Fragment {
 
+    int someVar;
+
+    TextView tvHello;
+
+    public static MainFragment newInstance(int someVar) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putInt("someVar", someVar);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        initInstance(rootView);
 
         return rootView;
 
 //        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private void initInstance(View rootView) {
+
+        tvHello = (TextView) rootView.findViewById(R.id.tvHello);
+
+    }
+
+    public void setHelloText(String text) {
+
+        tvHello.setText(text);
+
     }
 }
